688ef5ffc20fdd945884c7081e29be7e11ad6d01
filip <ja.lip132@gmail.com>
2021-08-24 19:50:42 +0200

    datatables

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,6 +1,51 @@
 <template>
   <div class="container">
-    <b-table striped hover :items="posts" :fields="fields"></b-table>
+    <b-container fluid>
+      <!-- User Interface controls -->
+      <b-row>
+        <b-col lg="12" class="my-1">
+          <b-col sm="6" class="my-1">
+            <b-form-group
+              label="Filter"
+              label-for="filter-input"
+              label-cols-sm="3"
+              label-align-sm="right"
+              label-size="sm"
+              class="mb-0"
+            >
+              <b-input-group size="sm">
+                <b-form-input
+                  id="filter-input"
+                  v-model="filter"
+                  type="search"
+                  placeholder="Type to Search"
+                ></b-form-input>
+              </b-input-group>
+            </b-form-group>
+          </b-col>
+
+          <b-col sm="6" class="my-1">
+            <b-pagination
+              v-model="currentPage"
+              :total-rows="totalRows"
+              :per-page="perPage"
+              align="fill"
+              size="sm"
+              class="my-0"
+            ></b-pagination>
+          </b-col>
+        </b-col>
+      </b-row>
+      <b-table
+        striped
+        hover
+        :items="posts"
+        :fields="fields"
+        :current-page="currentPage"
+        :per-page="perPage"
+        :filter="filter"
+      ></b-table>
+    </b-container>
   </div>
 </template>
 
@@ -16,20 +61,24 @@ export default {
       fields: [
         { key: "id", label: "Id" },
         { key: "userId", label: "User Id" },
-        { key: "title", label: "Title" },
-        { key: "body", label: "Body" },
-      ],
-      items: [
-        { age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-        { age: 21, first_name: "Larsen", last_name: "Shaw" },
-        { age: 89, first_name: "Geneva", last_name: "Wilson" },
-        { age: 38, first_name: "Jami", last_name: "Carney" },
+        { key: "title", label: "Title", sortable: true },
+        { key: "body", label: "Body", sortable: true },
       ],
       posts: [],
       postTitle: "",
       postBody: "",
       userId: 1,
+      totalRows: 1,
+      currentPage: 1,
+      perPage: 5,
+      pageOptions: [5],
+      filter: null,
+      filterOn: [],
     };
+  },
+  mounted() {
+    // Set the initial number of items
+    this.totalRows = this.posts.length;
   },
   async created() {
     try {
@@ -50,6 +99,10 @@ export default {
       this.postTitle = "";
       this.postBody = "";
     },
+  },
+  onFiltered(filteredItems) {
+    this.totalRows = filteredItems.length;
+    this.currentPage = 1;
   },
 };
 </script>
